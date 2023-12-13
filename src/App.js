12c1e041c6afe59e54cdfa17423b66cbe909a337
Celso Fabri Junior <celso.fabri@ifood.com.br>
2023-12-13 18:20:30 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import {
+  StyledContainer,
+  StyledSidebar,
+  StyledLogo,
+  StyledRanking,
+  StyledTitle,
+  StyledList,
+  StyledItem,
+  StyledPerson,
+  StyledPoints,
+  StyledEmptyState
+} from './styled.js';
+// import people from './mock.js';
+import fasterLogo from './assets/images/faster-logo.svg';
 
-function App() {
+const App = () => {
+
+  const [people, setPeople] = useState([])
+  const query = `
+    {
+      rankingCollection{
+        items {
+          name
+          points
+        }
+      }
+    }
+  `;
+
+  const ranking = people;
+
+  useEffect(() => {
+    window
+      .fetch(`https://graphql.contentful.com/content/v1/spaces/mgq7l530tefn/`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: 'Bearer cb4TdCZR4oyeLi4qQvyP3F3H_WnSd1rKsQkCNUBsyQE',
+        },
+        body: JSON.stringify({ query }),
+      })
+      .then((response) => response.json())
+      .then(({ data, errors }) => {
+        if (errors) {
+          console.error(errors);
+        }
+
+        setPeople(data.rankingCollection.items);
+      });
+    }, 
+  [query]);
+
+  ranking.sort((a, b) => {
+    return b.points - a.points;
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <StyledContainer>
+        <StyledSidebar>
+          <StyledLogo>
+            <img width="100%" height="auto" src={fasterLogo} alt="Faster" />
+            <p>Desafio Fitness</p>
+          </StyledLogo>
+        </StyledSidebar>
+        {ranking.length &&
+          (
+            <StyledRanking>
+              <StyledTitle>Ranking</StyledTitle>
+              <StyledList>
+                {ranking.map((person, index) => {
+                  return (
+                    <StyledItem key={index}>
+                      <StyledPerson>{person.name}</StyledPerson>
+                      <StyledPoints>{person.points}</StyledPoints>
+                    </StyledItem> 
+                  )
+                })}
+              </StyledList>
+            </StyledRanking>
+          )
+        }
+        {!ranking.length &&
+          <StyledEmptyState>
+            <p>No people yet.</p>
+          </StyledEmptyState>
+        }
+      </StyledContainer>
+    </React.Fragment>
   );
 }
 
---
+++ src/styled.js
@@ -0,0 +1,64 @@
+import styled from 'styled-components';
+
+export const StyledContainer = styled.main`
+    display: flex;
+    justify-content: flex-start;
+    flex-wrap: wrap;
+`;
+
+export const StyledSidebar = styled.aside`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 30%;
+    height: 100vh;
+    padding: 32px;
+    background-color: #0273cc;
+`;
+
+export const StyledLogo = styled.div`
+    font-size: 18px;
+    font-weight: 700;
+    font-style: italic;
+    color: #382b74;
+    text-align: center;
+    text-transform: uppercase;
+`;
+
+export const StyledRanking = styled.section`
+    width: 70%;
+    height: 100vh;
+    padding: 100px 32px;
+`;
+
+export const StyledTitle = styled.h3`
+    font-size: 32px;
+`;
+
+export const StyledList = styled.ul`
+    margin: 0;
+    padding: 0;
+    list-style-type: none;
+    border: 1px solid #e8e8e8;
+`;
+
+export const StyledItem = styled.li`
+    display: flex;
+    flex-start: flex-start;
+    flex-wrap: wrap;
+    padding: 16px;
+
+    &:nth-child(2n+1) {
+        background-color: #e8e8e8;
+    }
+`;
+
+export const StyledPerson = styled.div`
+    width: 70%;
+`;
+
+export const StyledPoints = styled.div`
+    width: 30%;
+`;
+
+export const StyledEmptyState = styled.div``;
